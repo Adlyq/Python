d8bad0728553e0beeb1a085a28a24d4724928621
adminbest <2833154405@qq.com>
2019-05-02 14:17:29 +0800

    test

---
+++ 人脸识别/rec.py
@@ -0,0 +1,60 @@
+import pickle
+import cv2 as cv
+import numpy as np
+import face_recognition as facerec
+from PIL import Image, ImageDraw, ImageFont
+
+class DATA:
+    def __init__(self, code, name):
+        self.face_code = code
+        self.face_name = name
+
+font = ImageFont.truetype("mcyh.ttf", 20, encoding="utf-8") # 参数1：字体文件路径，参数2：字体大小
+
+cap = cv.VideoCapture(0)
+
+gface_codes = []
+gface_names = []
+with open('facedatas', 'rb') as f: #load facedata
+    datas = pickle.load(f)
+    for data in datas:
+        gface_codes.append(data.face_code)
+        gface_names.append(data.face_name)
+
+
+while True:
+    ret, pic = cap.read()
+
+    cvimg = cv.cvtColor(pic, cv.COLOR_BGR2RGB)  # cv2和PIL中颜色的hex码的储存顺序不同
+    pilimg = Image.fromarray(cvimg)
+    draw = ImageDraw.Draw(pilimg)
+
+    faces = facerec.api.face_locations(pic, number_of_times_to_upsample=1, model='cnn')
+    face_codes = facerec.face_encodings(pic, faces, 1)
+    faces = [(left, top, right - left, bottom - top) for (top, right, bottom, left) in faces]
+
+    for (x, y, xs, ys), code in zip(faces, face_codes):
+        distances = facerec.face_distance(code, gface_codes)
+
+        resd = 999999
+        gstr = r'N/A'
+        res = -1
+        for i in range(len(distances)):
+            if resd >= distances[i] and distances[i] <= 0.38:
+                res = i
+                resd = distances[i]
+
+        if res > -1:
+            code = gface_codes[res]
+            name = gface_names[res]
+            gstr = name + '\n' + str(resd)
+
+        draw.rectangle((x, y, x + xs, y + ys), width=3)
+        draw.text((x, y - 50), gstr, (255, 0, 0), font=font)  # 参数1：打印坐标，参数2：文本，参数3：字体颜色，参数4：字体
+
+    cvcharimg = cv.cvtColor(np.array(pilimg), cv.COLOR_RGB2BGR)
+    cv.imshow("Face", cvcharimg)
+    key = cv.waitKey(30)
+    if key == 27:
+        break
+
---
+++ 人脸识别/reg.py
@@ -0,0 +1,43 @@
+import cv2 as cv
+import os
+import face_recognition as facerec
+import pickle
+
+class DATA:
+    def __init__(self, code, name):
+        self.face_code = code
+        self.face_name = name
+
+'''
+cap = cv.VideoCapture(0)
+ret, frame = cap.read()
+cv.imshow('aaa', frame)
+cv.waitKey()
+'''
+datas = []
+
+path = os.path.join(os.getcwd(), "./pic")
+for file in os.listdir(path):
+    fpath = os.path.join(path, file)
+    fname = file.split(".")[0]
+    pic = cv.imread(fpath)
+    faces = facerec.api.face_locations(pic, number_of_times_to_upsample=1, model='cnn')
+    print(faces, end='')
+    print('  ' + fname)
+    face_code = facerec.face_encodings(pic, faces, 1)
+    if len(face_code) != 1:
+        print(file +'  人脸数错误,已跳过')
+        continue
+    data = DATA(face_code[0], fname)
+    datas.append(data)
+
+with open('./facedatas', 'wb+') as f:
+    pickle.dump(datas,f)
+
+
+'''''
+with open('./facedatas', 'rb') as f:
+    datas = pickle.load(f)
+    for data in datas:
+        print(data.face_code)
+'''''
